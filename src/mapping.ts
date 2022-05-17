74edbe303ee46226f3622806f2117191eb5f5325
Le Yu <leyu00@gmail.com>
2022-05-17 16:08:40 -0400

    Add handleBondCreated logic. Add eslint, abi, and tests

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,15 +1,29 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  BondFactory,
-  BondCreated,
-  IssuerAllowListEnabled,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
-  TokenAllowListEnabled
-} from "../generated/BondFactory/BondFactory"
-import { ExampleEntity } from "../generated/schema"
+import { BondCreated } from '../generated/BondFactory/BondFactory';
+import { Bond as BondTemplate} from '../generated/templates/Bond/Bond';
+import { Bond, Dao, Token } from '../generated/schema';
 
 export function handleBondCreated(event: BondCreated): void {
+  const bond = new Bond(event.params.newBond.toHexString());
+  bond.name = event.params.name;
+  bond.amount = event.params.bonds;
+  bond.maturity = event.params.maturity;
+
+  // Check if the dao exists already
+  let dao = Dao.load(event.params.owner.toHexString());
+  if (!dao) {
+    dao = new Dao(event.params.owner.toHexString());
+  }
+  dao.name = 'Something Dao'; // TODO we need to add to the BondCreated event to get this
+  bond.dao = dao.id;
   
+  let paymentToken = Token.load(event.params.owner.toHexString());
+  if (!paymentToken) {
+    paymentToken = new Token(event.params.owner.toHexString());
+  }
+  bond.paymentToken = paymentToken.id;
+
+  bond.save();
+  dao.save();
+  paymentToken.save();
+  // BondTemplate.create();
 }
